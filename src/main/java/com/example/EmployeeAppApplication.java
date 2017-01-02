5b557d28b4619b2aaab920247fc69b82de825a15
Archana Pulikanti <Archana.Pulikanti@opusconsulting.com>
2017-01-02 17:36:01 +0530

    first commit

---
+++ src/main/java/com/example/EmployeeAppApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class EmployeeAppApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(EmployeeAppApplication.class, args);
+		 String[] beanNames = ctx.getBeanDefinitionNames();
+         
+	        Arrays.sort(beanNames);
+	         
+	        for (String beanName : beanNames) 
+	        {
+	            System.out.println(beanName);
+	        }
+	    }
+}

33e318641bc6c11095aef11b54d2226e0c2a4af1
Archana265 <Archana.Pulikanti@PD3020-FT6F622.OpusConsulting.com>
2017-01-24 16:25:38 +0530

    added hystrix dashboard, circuit breaker and turbine

--- src/main/java/com/example/EmployeeAppApplication.java
+++ src/main/java/com/example/EmployeeAppApplication.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrixDashboard
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableTurbine
 public class EmployeeAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/controllers/EmployeeRestController.java
+++ src/main/java/com/example/controllers/EmployeeRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Employee;
 import com.example.services.EmployeeService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class EmployeeRestController {
@@ -18,21 +19,29 @@ public class EmployeeRestController {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	@HystrixCommand(groupKey = "notification-service", fallbackMethod = "fallbackforcreateEmployee")
 	@PostMapping(value = "/rest/employee")
 	public ResponseEntity createEmployee(@RequestBody Employee employee) {
 		employeeService.saveEmployee(employee);
 		return new ResponseEntity(employee, HttpStatus.CREATED);
 	}
 	
+	@HystrixCommand(groupKey = "notification-service", fallbackMethod = "fallbackfordeleteEmployee")
 	@DeleteMapping("/rest/employeeDelete/{id}")
-	public ResponseEntity deleteCustomer(@PathVariable Long id) {
+	public ResponseEntity deleteEmployee(@PathVariable Long id) {
 		if (employeeService.getEmployeeById(id) == null ) {
 			return new ResponseEntity("No Employee found for ID " + id, HttpStatus.NOT_FOUND);
 		}else{
 		employeeService.deleteEmployee(id);
 		return new ResponseEntity(id, HttpStatus.OK);
 		}
-
+	}
+	public ResponseEntity fallbackfordeleteEmployee(@PathVariable Long id){
+		return new ResponseEntity("from fall back of fallbackfordeleteEmployee", HttpStatus.OK);
+	}
+	
+	public ResponseEntity fallbackforcreateEmployee(@RequestBody Employee employee){
+		return new ResponseEntity("from fall back of fallbackfordeleteEmployee", HttpStatus.OK);
 	}
 
 }
